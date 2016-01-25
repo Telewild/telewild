dcecc097cf7bb282cb4d6f113dbb192521ad5799
AliKhodadad <alikhodadad44@gmail.com>
2016-01-25 16:46:23 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {128035701,0,tonumber(our_id)},--Sudo users
+    sudo_users = {135693512,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
