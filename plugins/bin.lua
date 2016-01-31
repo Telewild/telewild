3e88466d64d38eef6f07f69c15b71580c81342d8
AliKhodadad <alikhodadad44@gmail.com>
2016-01-31 17:03:15 +0330

    Create bin.lua

---
+++ plugins/bin.lua
@@ -0,0 +1,28 @@
+-[[
+binsh by NXij
+Shell output for telegram-bot.
+]]—
+do
+function run(msg, matches)
+if is_sudo(msg) then
+return (matches[1]):read('*all')
+end
+end
+return {
+description = 'Run a system command.',
+usage = {
+sudo = {
+'!bin <command>\n'
+..'\nWARNING:'
+..'\nThis plugin interfaces with your operating system.'
+..'\nEven without root privileges, a bad command can be harmful.\n'
+..'\nOnly for sudoers. Turned off after use.',
+},
+},
+patterns = {
+'^bin (.*)$'
+},
+run = run,
+moderated = true
+}
+end
