5b14312f93d3b9e8101af2252752c821f314959f
AliKhodadad <alikhodadad44@gmail.com>
2016-02-03 16:09:29 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,6 +220,7 @@ function create_config( )
     "invite",
     "all",
     "leave_ban"
+    "echo"
     },
     sudo_users = {135693512,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
